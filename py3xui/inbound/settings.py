7a485685b12aac915024a52872d09ae62692de12
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 16:29:36 +0000

    Reset to c1388d48b8fee8ec1cd7764a133eec803f03558a and made changes for LLM Round 1

--- py3xui/inbound/settings.py
+++ py3xui/inbound/settings.py
@@ -1,12 +1,12 @@
 """
-This module provides classes for parsing JSON responses from the XUI API.
+This module contains the `Settings` class for parsing JSON responses from the XUI API.
 """
 
 from typing import List
 from py3xui.client.client import Client
 from py3xui.inbound.bases import JsonStringModel
 
-
+# pylint: disable=too-few-public-methods
 class SettingsFields:
     """Stores the fields returned by the XUI API for parsing."""
 
@@ -16,7 +16,7 @@ class SettingsFields:
 
 
 class Settings(JsonStringModel):
-    """Class for storing and parsing settings from the XUI API.
+    """Class representing settings for an inbound connection.
 
     Attributes:
         clients (List[Client]): List of Client objects related to inbound connections.
@@ -25,5 +25,10 @@ class Settings(JsonStringModel):
     """
 
     clients: List[Client] = []
+    """List of Client objects related to inbound connections."""
+
     decryption: str = ""
+    """String representing the decryption settings for inbound connections."""
+
     fallbacks: List = []
+    """List of fallbacks related to inbound connections."""
