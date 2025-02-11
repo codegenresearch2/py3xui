e0ae488566b534e82d71f2a3aa450f3bb4f1a085
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 15:31:03 +0000

    Reset to c1388d48b8fee8ec1cd7764a133eec803f03558a and made changes for LLM Round 1

--- py3xui/inbound/settings.py
+++ py3xui/inbound/settings.py
@@ -6,7 +6,7 @@ from typing import List
 from py3xui.client.client import Client
 from py3xui.inbound.bases import JsonStringModel
 
-
+# pylint: disable=missing-class-docstring
 class SettingsFields:
     """Stores the fields returned by the XUI API for parsing."""
 
@@ -15,6 +15,7 @@ class SettingsFields:
     FALLBACKS = "fallbacks"
 
 
+# pylint: disable=missing-docstring, too-few-public-methods
 class Settings(JsonStringModel):
     """Class for storing and parsing settings from the XUI API.
 
