63a2dd366195cbd1c4df4e62913890018a2848a1
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 16:01:26 +0000

    Reset to c1388d48b8fee8ec1cd7764a133eec803f03558a and made changes for LLM Round 1

--- py3xui/inbound/settings.py
+++ py3xui/inbound/settings.py
@@ -6,7 +6,7 @@ from typing import List
 from py3xui.client.client import Client
 from py3xui.inbound.bases import JsonStringModel
 
-
+# pylint: disable=too-few-public-methods
 class SettingsFields:
     """Stores the fields returned by the XUI API for parsing."""
 
